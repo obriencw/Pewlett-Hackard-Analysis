74fb431cce0a501b0601d38ef878ed434fce61b4
Koskie <pwdrkng@yahoo.com>
2021-08-16 15:25:25 -0500

    completed deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- DELIVERABLE 1
 -- Create Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
@@ -19,3 +20,17 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count(title) desc;
+
+
+-- DELIVERABLE 2
+-- Create a Mentorship Eligibility table for current employees who were born between January 1, 1965 and December 31, 1965
+SELECT DISTINCT ON (emp_no) e.emp_no, first_name, last_name, birth_date, de.from_date, de.to_date, title
+INTO mentorship_eligibility
+FROM employees e
+JOIN dept_emp de
+ON e.emp_no = de.emp_no
+JOIN titles t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
